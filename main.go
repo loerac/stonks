dbe228090f2cb0c42c312a6a2a3bb6adc7ae2b53
loerac <chatoloera@gmail.com>
2020-06-25 17:11:59 -0500

    Getting candlestick data
    Getting candlestick data so I can easily do some calculations.
    It still be pretty butt trash though....

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
     "encoding/json"
     "os"
     "time"
+    "strconv"
 )
 
 type Stocks struct {
@@ -20,23 +21,26 @@ type API struct {
     URL string  `json:"url"`
     Key string  `json:"key"`
 }
+
 type Quote struct {
-    Open    float64 `json:"o"`
-    Price   float64 `json:"c"`
-    High    float64 `json:"h"`
-    Low     float64 `json:"l"`
-    PrvClose float64 `json:"pc"`
-    Timestamp float64 `json:"t"`
-    Error   string  `json:"err"`
+    Prices  []float64 `json:"c"`
+    Volume  []int     `json:"v"`
+    High    []float64 `json:"h"`
+    Timestamp []float64 `json:"t"`
+    Status  string    `json:"s"`
+    Low     []float64 `json:"l"`
+    Open    []float64 `json:"o"`
+    Error   string    `json:"error"`
 }
 
 type Watchlist struct {
     Symbol      string
     CurrPrice   float64
+    Prices      []float64
     LmtPrice    float64
     Type        string
-    Average     []float64
-    Volume      int
+    Average     float64
+    Volume      []int
     Change      float64
     ChangePercent float64
 }
@@ -50,24 +54,12 @@ func calcAverage(arr []float64) float64 {
     return (sum / float64(len(arr)))
 }
 
-func (q *Quote) PrintQuote() {
-    if q.Error != "" {
-        fmt.Println("Error:     ", q.Error)
-    } else {
-        fmt.Println("Open:      ", q.Open)
-        fmt.Println("Price:     ", q.Price)
-        fmt.Println("High:      ", q.High)
-        fmt.Println("Low:       ", q.Low)
-        fmt.Println("Prev Close: ", q.PrvClose)
-        fmt.Println("Time:      ", q.Timestamp)
-    }
-}
-
 func (wl *Watchlist) printInfo() {
     fmt.Println("Ticker: " + wl.Symbol)
-    fmt.Println("--- Price:     ", wl.CurrPrice)
+    fmt.Println("--- Price:     ", wl.Prices[len(wl.Prices) - 1])
+    fmt.Println("--- Volume:    ", wl.Volume[len(wl.Volume) - 1], (wl.Volume[len(wl.Volume) - 2] - wl.Volume[len(wl.Volume) - 1]))
     fmt.Printf ("--- Change:     %.5f %.3f%%\n", wl.Change, wl.ChangePercent)
-    fmt.Printf ("--- Average:    %.5f\n", calcAverage(wl.Average))
+    fmt.Printf ("--- Average:    %.5f\n", wl.Average)
 }
 
 func main() {
@@ -103,23 +95,53 @@ func main() {
         watchlist[stocks[i].Ticker] = Watchlist{
             stocks[i].Ticker,
             0.0,
+            []float64{},
             stocks[i].Price,
             stocks[i].Type,
-            []float64{},
             0.0,
+            []int{},
             0.0,
             0.0,
         }
     }
 
     for ;; {
+        loc, _ := time.LoadLocation("America/New_York")
+        ts := time.Now().In(loc)
+        am := time.Date(
+            ts.Year(), ts.Month(), ts.Day(), 15, 30, 00, 0, time.UTC,
+        )
+        pm := time.Date(
+            ts.Year(), ts.Month(), ts.Day(), 4, 30, 00, 0, time.UTC,
+        )
+        om := time.Date(
+            ts.Year(), ts.Month(), ts.Day(), 9, 30, 00, 0, time.UTC,
+        )
+        cm := time.Date(
+            ts.Year(), ts.Month(), ts.Day(), 20, 0, 00, 0, time.UTC,
+        )
+        cm = cm.AddDate(0, 0, 1)
+
+        if ts.After(pm) && ts.Before(om) {
+            fmt.Println("Pre Market hours...")
+        } else if ts.After(om) && ts.Before(am) {
+            fmt.Println("Market Hours...")
+        } else if ts.After(am) && ts.Before(cm) {
+            fmt.Println("After Market hours....")
+        } else {
+            fmt.Println("Markets are closed...")
+        }
+
+        unixFrom := strconv.FormatInt(ts.AddDate(0, 0, -1).UTC().Unix(), 10)
+        unixTo := strconv.FormatInt(ts.UTC().Unix(), 10)
+
         for key, val := range watchlist {
             var quote Quote
-            url := api.URL + "quote?symbol=" + key + "&token=" + api.Key
+            url := api.URL + "stock/candle?symbol=" + key + "&resolution=1&from=" + unixFrom + "&to=" + unixTo + "&token=" + api.Key
 
             req, err := http.NewRequest("GET", url, nil)
             if nil != err {
-                fmt.Println("Couldn't set 'GET' request for " + key + "...skipping");
+                fmt.Println("--> Couldn't set 'GET' request for " + key + "...skipping");
                 fmt.Println(err)
                 continue
             }
@@ -127,7 +149,7 @@ func main() {
             req.Header.Add("cache-control", "no-cache")
             res, err := http.DefaultClient.Do(req)
             if nil != err {
-                fmt.Println("Couldn't get data for " + key + ": URL(" + url + ")...skipping")
+                fmt.Println("--> Couldn't get data for " + key + ": URL(" + url + ")...skipping")
                 fmt.Println(err)
                 continue
             }
@@ -137,14 +159,16 @@ func main() {
             json.Unmarshal(body, &quote)
             if quote.Error != "" {
                 fmt.Println("--> Couldn't get data for " + key +  "...skipping")
-                quote.PrintQuote()
+                fmt.Println("Error: " + quote.Error)
                 continue
             }
 
-            val.CurrPrice = quote.Price
-            val.Average = append(val.Average, quote.Price)
-            val.Change = val.CurrPrice - quote.PrvClose
-            val.ChangePercent = (val.Change / quote.PrvClose) * 100
+            val.CurrPrice = quote.Prices[len(quote.Prices) - 1]
+            val.Prices = quote.Prices
+            val.Volume = quote.Volume
+            val.Change = val.CurrPrice - quote.Prices[0]
+            val.ChangePercent = (val.Change / quote.Prices[0]) * 100
+            val.Average = calcAverage(quote.Prices)
             watchlist[key] = val
             val.printInfo()
         }
